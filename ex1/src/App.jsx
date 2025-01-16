cbf0270bbb624be2233f63d4ad6f7dafd24b7cac
Nanuli Mäntylä <morndulin@hotmail.com>
2025-01-16 16:45:38 +0200

    Exercises 1.3-1.5

--- ex1/src/App.jsx
+++ ex1/src/App.jsx
@@ -1,61 +1,75 @@
 /**
- *This small application showed how to use components and props to 
- make the code more versatile and simpler to use and modify. 
-
- The Array also clears up the code compared to listing all the information
- as different variables. 
+ * Small javaScript-example, that takes a "Course name" as a header and lists the different parts of the course, 
+ * the number of exercises per part and the total number of exercises.
+ *
  */
 
-// A functional component for displaying the course title
+/**
+ * Component that takes the header as a prop and returns it in correct form (as a header1)
+ * @returns Course name as a header
+ */
 const Header = (props) => {
-    // Receives 'course' as a prop and renders it inside an <h1> element
-    return <h1>{props.course}</h1>;
-};
+    return <h1>{props.course}</h1>
+}
 
-// A functional component for displaying a single part of the course
+/**
+ * Component that renders one part name and the amount of exercises
+ * @returns name of the course and the number of exercises
+ */
+const Part = (props) => {
+    return (
+        <p>{props.part} {props.exercises}</p>
+    )
+}
+
+/**
+ * Component that takes all the parts as a props and renders all three Part-components, one for each part of the course.
+ * @returns all three parts
+ */
 const Content = (props) => {
-    // Receives 'part' and 'exercises' as props and renders them in a <p> element
-    return <p>{props.part} {props.exercises}</p>;
-};
-
-// A functional component for calculating and displaying the total number of exercises
-const Total = (props) => {
-    // Calculates the total by summing the three numbers passed as props
-    const total = props.num1 + props.num2 + props.num3;
-    // Renders the total inside a <p> element
-    return <p>Total number of exercises: {total}</p>;
-};
-
-// The main App component that combines all other components
-const App = () => {
-    // The course name
-    const course = 'Half Stack application development';
-
-    // An array of objects representing parts of the course, each with a name and the number of exercises
-    const parts = [
-        { part: 'Fundamentals of React', ex: 10 },
-        { part: 'Using props to pass data', ex: 7 },
-        { part: 'State of a component', ex: 14 },
-    ];
-
-    // The main JSX structure rendered by the App component
     return (
         <div>
-            {/* Pass the course name to the Header component */}
-            <Header course={course} />
-
-            {/* Render each part of the course by passing its details to the Content component */}
-            <Content part={parts[0].part} exercises={parts[0].ex} />
-            <Content part={parts[1].part} exercises={parts[1].ex} />
-            <Content part={parts[2].part} exercises={parts[2].ex} />
-
-            {/* Pass the number of exercises for each part to the Total component */}
-            <Total num1={parts[0].ex} num2={parts[1].ex} num3={parts[2].ex} />
+            <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
+            <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
+            <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
         </div>
     );
 };
 
-export default App; // Exports the App component so it can be used in other parts of the project
+/**
+ * Component that counts the total sum of the exercises. 
+ * @returns the total number of exercises
+ */
+const Total = (props) => {
+    const total =
+        props.parts[0].exercises +
+        props.parts[1].exercises +
+        props.parts[2].exercises;
+
+    return <p>Total number of exercises: {total}</p>
+}
+
+/**
+ * The main component that is exported to main
+ */
+const App = () => {
+    const course = 'Half Stack application development'
+    const parts = [
+        { name: 'Fundamentals of React', exercises: 10 },
+        { name: 'Using props to pass data', exercises: 7 },
+        { name: 'State of a component', exercises: 14 },
+    ]
+
+    return (
+        <div>
+            <Header course={course} />
+            <Content parts={parts} />
+            <Total parts={parts} />
+        </div>
+    )
+}
+
+export default App
 
 /**
  * The original version of the code: 
